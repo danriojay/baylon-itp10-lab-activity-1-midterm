6e7af63933e4611b590bfbf3ead6eb1b0b454593
Danrio Baylon <baylondl.856.stud@udd.edu.ph>
2026-09-18 09:59:19 +0800

    testing

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const profileCard = document.querySelector("#profileCard");
+
+const profileName = document.getElementById("profileName");
+const profileProgram = document.getElementById("profileProgram");
+const profileYear = document.getElementById("profileYear");
+const profileStatus = document.getElementById("profileStatus");
+
+const detailsPanel = document.getElementById("detailsPanel");
+const studentIdDisplay = document.getElementById("studentIdDisplay");
+
+const nameInput = document.getElementById("nameInput");
+const programInput = document.getElementById("programInput");
+const yearInput = document.getElementById("yearInput");
+const statusInput = document.getElementById("statusInput");
+
+const updateBtn = document.getElementById("updateBtn");
+const toggleDetailsBtn = document.getElementById("toggleDeailsBtn");
+const themeBtn = document.getElementById("themeBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+const formMessage = document.getElementById("formMessage");
+
+const INITIAL_NAME = "Maria Santos";
+const INITIAL_PROGRAM = "BS Information Technology";
+const INITIAL_YEAR = "3rd Year";
+const INITIAL_STATUS = "ACTIVE";
